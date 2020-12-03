6de1f4467dd442d7a937994d353badd77ce1caf8
Matej <matej@hamradio.si>
2020-12-03 17:59:25 +0100

    * Ported prg.py to python3, removed hex file support in favor of binary

--- py/prg.py
+++ py/prg.py
@@ -1,87 +1,64 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-# avr isp bub hex/bin programmer, Matej Kogovsek (matej@hamradio.si), 16.mar.2017
-# adapted from bus gofer prg
-# requires pySerial, http://pyserial.sourceforge.net
-# and intelhex, http://www.bialix.com/intelhex/
+import sys,serial,time,crcmod
 
-import sys,serial,datetime,intelhex,crc16pure
-
-SER_SPEED = 4800
+#def atcmd(cmnd, resp, to):
+#  print(cmnd)
+#  return 'OK'
 
 def atcmd(cmnd, resp, to = 0.5):
   if ser.timeout != to:
     ser.timeout = to
   ser.flushInput()
-  ser.write(cmnd + '\n')
-  r = ser.readline().rstrip();
+  ser.write((cmnd + '\n').encode('ascii'))
+  r = ser.readline().decode('ascii').rstrip();
   if len(resp) > 0 and r.find(resp) == -1:
     if r == '': r = '(none)'
-    raise Exception('Error! expected ' + resp + '\ncmnd was: ' + cmnd + '\nresp was: ' + r + '\n')
+    raise RuntimeError('Error! expected ' + resp + '\ncmnd was: ' + cmnd + '\nresp was: ' + r + '\n')
   return r
 
 if len(sys.argv) < 3:
-  print 'usage: prg.py serial_if filename'
-  sys.exit(1)
+  print('usage: prg.py serial_if filename')
+  exit(1)
 
-sif = sys.argv[1]
-fn = sys.argv[2]
-pgw_size = 64
+f = open(sys.argv[2], 'rb')
+b = f.read()
+f.close()
+print('text size:',len(b))
+xmodem_crc_func = crcmod.mkCrcFun(0x11021, rev=False, initCrc=0x0000, xorOut=0x0000)
+bcrc = xmodem_crc_func(b)
 
+ser = serial.Serial(sys.argv[1], 4800)
 try:
-  h = intelhex.IntelHex()
-  if fn[-3:] == 'hex':
-    h.loadhex(fn)
-  else:
-    h.loadbin(fn)
-except intelhex.HexReaderError, e:
-  print "Bad hex file\n", str(e)
-  sys.exit(1)
-
-ser = serial.Serial(sif, SER_SPEED)
-try:
-  for i in range(5):
+  retries = 5
+  while retries:
     try:
+      retries -= 1
       atcmd('AT+BUFRDDISP=0', 'OK')
       break
     except:
       pass
-  if i == 4:
-    print 'avr isp bub not responding'
-    sys.exit(1)
+  if retries == 0:
+    print('avr isp bub not responding')
+    exit(1)
 
-  hna = h.minaddr() # veeeeeery slow function
-  hma = h.maxaddr() # same
-  a = hna
-  sdt = datetime.datetime.now()
-  tb = 0
-  crc = 0
-  while a < hma:
-    s = ''
-    adr = hex(a)[2:].zfill(6)
-    nb = min(pgw_size, hma - a + 1)
-    tb = tb + nb
-    for i in range(nb):
-      s = s + hex(h[a])[2:].zfill(2)
-      a = a + 1
-    crc = crc16pure.crc16xmodem(s.decode('hex'), crc)
-    try:
-      te = datetime.datetime.now() - sdt # time elapsed
-      pd = 1.0 * (a-hna) / (hma-hna) # prg done
-      tl = datetime.timedelta(seconds = int(((1.0-pd)/pd) * te.total_seconds())) # time left
-      print adr,nb,tb,'/',hma-hna+1,'(',int(100.0*pd),'%)',tl,' left'
-      atcmd('AT+BUFWR=' + s, 'OK')
-      atcmd('AT+EE24WR=' + adr, 'OK')
-      atcmd('AT+EE24RD=' + adr + ',' + str(i + 1), 'OK')
-      atcmd('AT+BUFCMP', 'OK')
-    except Exception, e:
-      print e
-      break
-  print
-  if hna == 0:
-    print 'Device CRC:',atcmd('AT+EE24CRC=' + str(hma+1), '', 20)
-    print 'File CRC  :',hex(crc)[2:]
+  addr = 0
+
+  while addr < len(b):
+    nb = min(64, len(b)-addr)
+    s = b[addr:addr+nb].hex()
+    print(addr,'/',len(b))
+    atcmd('AT+BUFWR={}'.format(s), 'OK')
+    atcmd('AT+EE24WR={:06x}'.format(addr), 'OK')
+    atcmd('AT+EE24RD={:06x},{}'.format(addr,nb), 'OK')
+    atcmd('AT+BUFCMP', 'OK')
+    addr += nb
+
+  dcrc = atcmd('AT+EE24CRC={}'.format(len(b)), '', 20)
+  print('Device CRC:',dcrc)
+  print('File CRC  :',hex(bcrc)[2:])
+  print('Done.')
+except Exception as e:
+  print(str(e))
 finally:
   ser.close()
-
-print 'Done.'
